2ed3a7e0a4ec5d62140facc8d925964a5a432e0d
rvvasilevv <114993007+rvvasilevv@users.noreply.github.com>
2022-12-22 14:10:30 +0200

    Add files via upload

---
+++ Sqlwithpython/main.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="123brawo123",
+  database="pythondb"
+)
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO imenta(id,name,city) VALUES (1,'Georgi','Sofia')"
+mycursor.execute(sql)
+mydb.commit()
+
+print(mycursor.rowcount, "records deleted")
